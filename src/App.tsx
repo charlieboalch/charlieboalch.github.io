300c1853fbadebc056d8fcc2b299351cee019eda
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2025-07-24 11:44:25 -0400

    Update favicon and website title

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,26 @@ const ContentView = styled('div')`
     padding: 0 15% 0 15%;
     overflow: hidden;
     max-height: 100vh;
-    
+
     @media screen and (max-width: 768px) {
         flex-direction: column;
         padding: 0 2% 0 2%;
     }
-    
+
     @media screen and (max-width: 1080px) {
         padding: 0 2% 0 2%;
     }
 `
 
 function App() {
-  return (
-    <ContentView>
-      <BioComponent/>
-      <InfoComponent/>
-    </ContentView>
-  )
+    return <>
+        <title>Charlie Boalch | Home</title>
+        <ContentView>
+
+            <BioComponent/>
+            <InfoComponent/>
+        </ContentView>
+    </>
 }
 
 export default App
