540678d9b2be6e2971afa8d33ca61c1c2c41ceed
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2026-04-30 22:31:25 -0400

    Update projects with new logistic regression one

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -5,15 +5,26 @@ import {ProjectProps} from "./components/projects/project.types";
 // TODO: add images to PetVR section
 const projectsData: ProjectProps[] = [
     {
+        title: "Swimming Finals Predictor",
+        description: [
+            "As part of my STAT 4210H class, I made a model that predicts the odds of a swimmer making finals in their event using data from the 2026 College Club Nationals meet. I used to swim, and this was an interesting exercise in fitting models and using them with real world data.",
+            "I scraped the data from the website using a Python script (over 3500 data points!) and did the rest of the analysis in R. The model uses multivariate logistic regression with five predictors. The results of the regression fitting revealed that factors like heat, gender, event, and time improvement were all significant influences on the odds of a swimmer making finals.",
+            "The model was also accurate (almost surprisingly so). Generating a confusion matrix of the test set revealed high accuracy and sensitivity, and moderate specificity. The model also appeared to be very well calibrated, though tending towards slight underconfidence in upper probabilities. We figured that this was because most of the sample didn't make finals, causing the model to lean more conservative when making predictions.",
+            "I learned a lot from this project and it made me realize that this field is something that I want to pursue further. I'll be looking into analyzing more data and fitting more complex models in the future."
+        ],
+        images: ["/projects/finals-3.png", "/projects/finals-1.png", "/projects/finals-2.png"],
+        reversed: false
+    },
+    {
         title: "Song to Movie",
         description: [
             "Song to Movie is a website that gives the most similar movies to a given song. I wanted to combine my love for music and film in a fun and interactive way.",
             "Media is scored on six different dimensions - mood, energy, grittiness, tension, romance, and humor. These are extracted from movie plots and song lyrics using SBERT and roBERTa, and fine-tuned by genre, user tags, and Spotify's audio feature data. Song vectors are matched to movies using cosine and L2 similarity searches.",
             "Part of this project was inspired by prior research from UC Berkeley (https://www.ischool.berkeley.edu/projects/2023/moviemood). I thought their approach was flawed, however, because they only used 4 dimensions and their mood vectors were generated using LLMs. I aimed to improve on their approach by using more deterministic models that specialized in emotion embeddings.",
-            "I hosted this project on my website at https://song.phqsh.me as a demo. The interface is built using React and Material UI. Parts of the site are disabled due to Spotify's restrictions on account authorization."
+            "The web interface is built using React and Material UI. Parts of the site are disabled due to Spotify's (annoying) restrictions on account authorization."
         ],
         images: ["/projects/song-movie-1.png", "/projects/song-movie-2.png", "/projects/song-movie-3.png"],
-        reversed: false
+        reversed: true
     },
     {
         title: "UGA Dining Analysis",
@@ -23,7 +34,7 @@ const projectsData: ProjectProps[] = [
             "Once I have enough data, I plan to run an analysis to reveal trends, forecast usage patterns, and build a model of the typical day at the dining halls. ",
         ],
         images: ["/projects/uga-1.png"],
-        reversed: true
+        reversed: false
     },
     {
         title: "CREWS",
@@ -40,18 +51,6 @@ const projectsData: ProjectProps[] = [
             "/projects/crews-3.png",
             "/projects/crews-4.png",
             "/projects/crews-6.png"
-        ],
-        reversed: false
-    },
-    {
-        title: "PetVR",
-        description: [
-            "PetVR is a virtual reality application for Oculus that helps connect potential pet adopters with rescue animals. It places the adopter in an immersive environment where they can interact with the animal before they meet them.",
-            "I built the frontend experience in Unity, integrating RESTful APIs to fetch data on adoptable pets. I focused on providing a balance between displaying information to the user and making the interactions feel natural and responsive.",
-            "On the backend, I used the Actix Web framework for Rust to provide a middle layer between the frontend and the external API provider. The backend also handled the caching of API responses and scaling to meet load requirements.",
-            "All models used in PetVR were found on Thingiverse, but the actual construction of the environment was all done by hand. PetVR won awards for Virtual Reality Simulation at the 2023 National TSA Conference in Louisville."
-        ],
-        images: [
         ],
         reversed: true
     }
